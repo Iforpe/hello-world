56f4a1e995eca37404dece6f3f15065806d3fc70
Iforpe <dmitrybelomutov@gmail.com>
2019-09-12 04:39:27 +0300

    ft_strdup (final)

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,15 +1,39 @@
 #include <stdlib.h>
 
-char	*ft_strdup(char *src)
+int		ft_strlen(char *str)
 {
-	char *str;
-
 	int length;
-	while (*src[length])
-		length++
-	if (!(char *)malloc(sizeof(char) * (length + 1)))
-		return (NULL);
-	str[length] = '\0';
-	while (length-- >= 0)
-		str[length] = src[length];
-	return (str);
+
+	length = 0;
+	while(str[length])
+	{
+		str++;
+		length++;
+	}
+	return(length);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	return(dest);
+}
+
+char	*ft_strdup(char *str)
+{
+	char *dup;
+
+	dup = (char *)malloc(sizeof(*dup) * (ft_strlen(str) + 1));
+	dup = ft_strcpy(dup, str);
+	return (dup);
+}
